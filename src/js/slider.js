ef840dc33cab266e5863954e00b88788429d2c6e
katatonia <pmilekhina@gmail.com>
2024-03-05 15:47:08 +0400

    create slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,11 +1,13 @@
 const slider = () => {
     const sliderBtns = document.querySelectorAll('.slider__btn');
-    const sliderList = document.querySelector('.slider__list ');
+    const sliderList = document.querySelector('.slider__list');
+    const sliderItem = document.querySelector('.slider__item');
 
     sliderBtns.forEach(btn => {
         btn.addEventListener('click', () => {
-            const direction = btn.classList === 'slider__btn_prev' ? -1 : 1;
+            const direction = btn.id === 'slider__btn_prev' ? -1 : 1;
             const scrollAmount = sliderList.clientWidth * direction;
+            console.log(scrollAmount);
             sliderList.scrollBy({ left: scrollAmount, behavior: 'smooth' });
         });
     });
