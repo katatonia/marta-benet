6da04b041f2a31110ad3957f86ec429a9277c9a0
katatonia <pmilekhina@gmail.com>
2024-03-13 11:16:01 +0400

    optimizes code

--- src/js/humburger.js
+++ src/js/humburger.js
@@ -3,29 +3,20 @@ const nav = document.querySelector('.nav');
 const header = document.querySelector('.header');
 
 const openNav = () => {
-
     humburger.addEventListener('click', () => {
-        if (nav.classList.contains('nav_opened')) {
-            nav.classList.remove('nav_opened');
-            humburger.classList.remove('nav__humburger_active');
-        } else {
-            nav.classList.add('nav_opened');
-            humburger.classList.add('nav__humburger_active');
-        }
+        nav.classList.toggle('nav_opened');
+        humburger.classList.toggle('nav__humburger_active');
     });
 };
 
-const onScrollCloses = () => {
-
+const onScrollClosing = () => {
     window.addEventListener('scroll', () => {
-        if (nav.classList.contains('nav_opened')) {
-            if (window.scrollY >= header.clientHeight) {
-                nav.classList.remove('nav_opened');
-                humburger.classList.remove('nav__humburger_active');
-            }
+        if (nav.classList.contains('nav_opened') && window.scrollY >= header.clientHeight) {
+            nav.classList.toggle('nav_opened', false);
+            humburger.classList.toggle('nav__humburger_active', false);
         }
     });
 };
 
 openNav();
-onScrollCloses();
+onScrollClosing();
